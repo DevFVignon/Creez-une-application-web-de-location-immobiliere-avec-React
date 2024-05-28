6594e5c776a1c53640523931cf9ee52315941ca9
FVignon <florentvignon.developpeur@gmail.com>
2024-05-28 16:05:02 +0200

    ajout commentaire sur composant banner

--- src/components/Banner_temp.jsx
+++ src/components/Banner_temp.jsx
@@ -1,5 +1,8 @@
 import './Banner.css'
 
+// Les props attendues sont :
+// 1)l'image sous cette forme dans le composant : ' picture={require("../chemin_de_l_image/image.extension")} '
+// 2)le text sur l'image sous cette forme :' text="texte à insérer" '
 function Banner(props) {
     return(  
         <div className='banner-container'>
