9c29dfcb11ef6e03f9c6643aeed3ac928aa30c9d
FVignon <florentvignon.developpeur@gmail.com>
2024-06-03 18:21:58 +0200

    lien dans le nav souligné quand l'utilisateur est sur la page concerné

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,15 +1,16 @@
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 import logo from '../../assets/LOGO.png'
 import './Header.css';
 
 function Header() {
+    const location = useLocation();
     return (
         
         <nav className="nav">
             <img src={logo} alt="Logo Kasa" className="nav-logo" />
             <div className="nav-links">
-                <Link to="/">Accueil</Link>
-                <Link to='/about'>A propos</Link>
+                <Link to="/"  className={location.pathname === '/' ? 'active' : ''}>Accueil</Link>
+                <Link to='/about' className={location.pathname === '/about' ? 'active' : ''}>A propos</Link>
             </div>
         </nav>
     )
