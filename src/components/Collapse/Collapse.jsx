b482a8fcc3816a7baf95dfc645ffc07f05b68616
FVignon <florentvignon.developpeur@gmail.com>
2024-06-03 09:44:20 +0200

    Animation du collapse

--- src/components/Collapse/Collapse.jsx
+++ src/components/Collapse/Collapse.jsx
@@ -1,30 +1,42 @@
-import './Collapse.css'
-import { useState } from 'react';
-import arrowUp from '../../assets/arrow_up.png'
-import arrowDown from '../../assets/arrow_down.png'
+import './Collapse.css';
+import { useState, useRef } from 'react';
+import arrowUp from '../../assets/arrow_up.png';
+import arrowDown from '../../assets/arrow_down.png';
 
-function Collapse(props){
-    const [isOpen, setisOpen]=useState(false)
-    return !isOpen ? (<div className="collaspe">
-                            <div className='collapse-header'>
-                                <h2>{props.title}</h2> <button className='collapse-button' onClick={()=> setisOpen(true)}><img src={arrowUp} alt='Déplier'></img></button> 
-                            </div> 
-                    </div>) :
-                    (<div className="collaspe">
-                            <div className='collapse-header'><h2>{props.title}</h2> <button className='collapse-button' onClick={()=> setisOpen(false)}><img src={arrowDown} alt="Replier"></img></button></div>
-                            
-                            { props.content ? 
-                            <p>{props.content}</p> : null
-                            }
-                            
-                            {props.list ? 
-                            <ul>
-                                {props.list.map((element, index) => (
-                                    <li key={index}>{element}</li>
-                                ))}
-                            </ul> : null}  
-                    </div>)
+function Collapse(props) {
+    const [isOpen, setIsOpen] = useState(false);
+    const contentRef = useRef(null);
+
+    const toggleCollapse = () => {
+        setIsOpen(!isOpen);
+    };
+
+    return (
+        <div className="collapse">
+            <div className='collapse-header'>
+                <h2>{props.title}</h2>
+                <button className='collapse-button' onClick={toggleCollapse}>
+                    <img src={isOpen ? arrowDown : arrowUp} alt={isOpen ? 'Replier' : 'Déplier'} />
+                </button>
+            </div>
+            <div
+                ref={contentRef}
+                className="collapse-content"
+                style={{
+                    height: isOpen ? `${contentRef.current.scrollHeight}px` : '0px'
+                }}
+            >
+                {props.content && <p>{props.content}</p>}
+                {props.list && (
+                    <ul>
+                        {props.list.map((element, index) => (
+                            <li key={index}>{element}</li>
+                        ))}
+                    </ul>
+                )}
+            </div>
+        </div>
+    );
 }
 
 export default Collapse;
-
