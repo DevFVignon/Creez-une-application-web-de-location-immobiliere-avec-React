ad6759f7bd991d7b45afce760b5486e2ffde93e3
FVignon <florentvignon.developpeur@gmail.com>
2024-06-17 16:43:08 +0200

    Composant nav créé pour être utilisé dans header. Sémantique et documentation header et nav ok

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -2,31 +2,21 @@
  * Header composant
  * 
  * Il représente l'en-tête de l'application Kasa.
- * Il affiche le logo de Kasa et des liens de navigation vers les pages Accueil et A propos.
+ * Il affiche le logo de Kasa et, grâce au composant Nav, des liens de navigation vers les pages Accueil et A propos.
  * Utilisation de la bibliothèque react-router-dom pour la navigation.
  * 
  * @returns {JSX.Element} Forme du rendu du composant.
  */
 
-import { Link, useLocation } from "react-router-dom";
 import logo from '../../assets/LOGO.png'
 import './Header.css';
+import Nav from '../Nav/Nav'
 
 function Header() {
-    const location = useLocation();
     return (
         <header className="header">
             <img src={logo} alt="Logo Kasa" className="header-logo" />
-            <nav className="nav">
-                <ul className="nav-links">
-                    <li>
-                        <Link to="/"  className={location.pathname === '/' ? 'active' : ''}>ACCUEIL</Link>
-                    </li>
-                    <li>
-                        <Link to='/about' className={location.pathname === '/about' ? 'active' : ''}>A PROPOS</Link>
-                    </li>
-                </ul>
-            </nav>
+            <Nav/>
         </header>
     )
 }
---
+++ src/components/Nav/Nav.jsx
@@ -0,0 +1,31 @@
+/**
+ * Nav Composant
+ * 
+ * Composant de navigation pour l'application Kasa.
+ * Affiche les liens de navigation vers les pages Accueil et A propos.
+ * Utilisation de la bibliothèque react-router-dom pour la navigation.
+ * 
+ * @returns {JSX.Element} Forme du rendu du composant.
+ */
+
+import { Link, useLocation } from "react-router-dom";
+import './Nav.css'
+
+function Nav(){
+    const location = useLocation();
+
+    return(
+        <nav className="nav">
+        <ul className="nav-links">
+            <li>
+                <Link to="/"  className={location.pathname === '/' ? 'active' : ''}>ACCUEIL</Link>
+            </li>
+            <li>
+                <Link to='/about' className={location.pathname === '/about' ? 'active' : ''}>A PROPOS</Link>
+            </li>
+        </ul>
+        </nav>
+    )
+}
+
+export default Nav; 
