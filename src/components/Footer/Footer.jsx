c5c99221497362108dc7939d55d7bb3ad8496653
FVignon <florentvignon.developpeur@gmail.com>
2024-06-17 14:10:31 +0200

    Footer : documentation et sémantique ok

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,10 +1,19 @@
+/**
+ * Footer composant
+ * 
+ * Il représente le pied de page de l'application Kasa.
+ * Il affiche le logo en noir et blanc de Kasa et un message de droits réservés.
+ * 
+ * @returns {JSX.Element} Forme du rendu du composant.
+ */
+
 import './Footer.css';
 import logoBW from '../../assets/LOGO_B&W.png';
 
 function Footer() {
     return(
         <footer className='footer'>
-            <img src={logoBW} alt="Kasa" />
+            <img src={logoBW} alt="Logo de Kasa en noir et blanc" />
             <p>© 2020 Kasa. All rights reserved</p>
         </footer>
     )
